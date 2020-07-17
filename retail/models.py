b2a414ea0fa4e613af83a4d279f423ec05f1eeb7
Prathyusha <prathyushapatel840@gmail.com>
2020-07-17 16:13:08 +0530

    added products model

--- retail/models.py
+++ retail/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     phonenum = models.CharField(max_length=13)
     password = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
